a1f0e00d88405deee91b33b8f51edf583e5bf39b
ulempicka <urszula.lempicka@gmail.com>
2020-06-22 08:19:19 +0200

    commit 2 : simplified conditional statement

--- src/triangle/Type.java
+++ src/triangle/Type.java
@@ -3,11 +3,7 @@ package triangle;
 public class Type {
     boolean isRightTriangle(Triangle triangle){
         boolean isRight = (triangle.a * triangle.a) + (triangle.b * triangle.b) == triangle.c * triangle.c;
-        boolean condition = isRight && triangle.c > triangle.a && triangle.c > triangle.b;
-        if(condition){
-            return true;
-        }else
-            return false;
+        return isRight && triangle.c > triangle.a && triangle.c > triangle.b;
     }
 
     void notify(Triangle triangle){
